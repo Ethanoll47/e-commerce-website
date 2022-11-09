2ccd63c90e3d93e5ebaf3b0d73b2a5fa0886530e
BBR0XX <116264199+BBR0XX@users.noreply.github.com>
2022-11-09 20:05:41 +0800

    Added function navbar

--- component.php
+++ component.php
@@ -1,4 +1,37 @@
-<?
+<?php
+
+function navbar(){
+    echo '<nav class="navbar navbar-expand-lg bg-dark navbar-dark py-3 fixed-top">
+        <div class="container">
+            <a href="#" class="navbar-brand order-lg-0">Company Name</a>
+
+            <div class="nav-buttons order-lg-2">
+                <button type="button" class="btn position-relative pt-0"><i class="bi bi-cart fs-4 text-secondary"></i></button>
+                <button type="button" class="btn position-relative pt-0"><a href="login.html"></a><i class="bi bi-person-circle fs-4 text-secondary"></i></a></button>
+            </div>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navmenu">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse order-lg-1" id="navmenu">
+                <ul class="navbar-nav mx-auto text-center">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link px-4 py-2">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#products" class="nav-link px-4 py-2">Products</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#about" class="nav-link px-4 py-2">About Us</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>';
+}
+    
+
 function cart(){
 //Get method for adding/remove item to Cart
 if(!empty($_GET["action"])) {
