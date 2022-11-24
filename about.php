5f0e1d7a903cbdd5613ddf2d69c166ed5eb7dd45
Ethanoll47 <116264100+Ethanoll47@users.noreply.github.com>
2022-11-24 19:01:06 +0800

    Updated About page

--- about.php
+++ about.php
@@ -19,8 +19,43 @@ require_once("php/config.php");
     <title>Company Name</title>
 </head>
 <body>
-   
-    
+    <!-- About Us -->
+    <section class="p-5">
+        <div class="container text-center">
+            <h2 class="mb-4">About Us</h2>   
+            <div class="d-flex flex-wrap justify-content-center">
+                Thrifted is a Malaysian-based company started in 2022 that sells second-hand clothing. According to the Ellen Macarthur Foundation, 60% of average consumers throw away their clothes in the first year, and an estimated 18.6 million tonnes of clothing will end up in landfills.  Hence, we aim to provide customers with the best online shopping experience while also reducing the number of clothes that end up in landfills.
+            </div>
+            
+        </div>
+    </section>
+
+    <!-- Our Mission -->
+    <section class="p-5 bg-light">
+        <div class="container">
+            <div class="row align-items-center justify-content-between">
+                <div class="d-flex flex-column flex-md-row">
+                    <h2 class="mx-4">Our Mission</h2>
+                <p class="mx-4">
+                    Thrifted’s mission is to reduce the number clothes that end up in landfills by reselling second-hand clothes and powering the circular economy for fashion. All our clothes are carefully collected, sorted, cleaned before being sold on our website. This ensures customers will get the highest quality clothes while also ensuring a more sustainable environment. 40% of all Thrifted’s profits will be donated to Fashion Revolution Malaysia in support of their efforts to change the way clothing is produced, sourced, and consumed.
+                </p>
+            </div>
+        </div>
+    </section>
+
+    <!-- Our Vision -->
+    <section class="p-5">
+        <div class="container text-center">
+            <h2 class="mb-4">Our Vision</h2>   
+            <div class="d-flex flex-wrap justify-content-center">
+                To clean up the fashion industry and create a more sustainable environment. 
+            </div>
+            
+        </div>
+    </section>
+<?php
+    footer();
+    ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
